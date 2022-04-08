6f2ebd16f9e2132bf9a1069a0426d8dd4864cde5
ad0x99 <anhduc.dev99@gmail.com>
2022-04-08 13:42:52 +0700

    feat: reverse string

--- The Coding Interview Bootcamp Algorithms & Data Structures/exercises/reversestring/index.js
+++ The Coding Interview Bootcamp Algorithms & Data Structures/exercises/reversestring/index.js
@@ -6,6 +6,44 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// Solution 1
+/**
+ * We split the string into an array, reverse the array, and then join the array back into a string
+ * @param str - The string to be reversed
+ * @returns The reverse of the string
+ */
+const reverse = (str) => {
+  const arr = str.split('');
+  arr.reverse();
+  return arr.join('');
+};
 
-module.exports = reverse;
+// Solution 2
+/**
+ * We loop through the string and push each character to the beginning of the reversedStr variable
+ * @param str - The string to be reversed
+ * @returns The reversed string
+ */
+const reverse2 = (str) => {
+  let reversedStr = '';
+
+  for (const st of str) {
+    if (st.length > 0) {
+      reversedStr = st + reversedStr;
+    }
+  }
+  return reversedStr;
+};
+
+// Solution 3
+/**
+ * We split the string into an array of characters, then we reduce the array into a string, and we add
+ * each character to the beginning of the string
+ * @param str - the string to be reversed
+ * @returns the reversed string.
+ */
+const reverse3 = (str) => {
+  return str.split('').reduce((rev, char) => char + rev, '');
+};
+
+module.exports = { reverse, reverse2, reverse3 };
