acef60ca0572e60d418efbfa05ceccc19d6e4ddd
ad0x99 <anhduc.dev99@gmail.com>
2022-08-27 15:34:32 +0700

    solved: reverse integer

--- The Coding Interview Bootcamp Algorithms & Data Structures/exercises/reverseint/index.js
+++ The Coding Interview Bootcamp Algorithms & Data Structures/exercises/reverseint/index.js
@@ -8,6 +8,33 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// Solution 1
+
+/**
+ * We convert the number to a string, split it into an array, filter out any zeros, reverse the array,
+ * join it back into a string, and then convert it back to a number
+ * @param n - the number to be reversed
+ * @returns the reversed integer.
+ */
+const reverseInt = (n) => {
+  const isPositiveNumber = Math.sign(n) === 1;
+  const isNegativeNumber = Math.sign(n) === -1;
+  const reversedInt = parseInt(n.toString().split('').reverse().join(''));
+
+  return isPositiveNumber ? reversedInt : isNegativeNumber ? -reversedInt : n;
+};
+
+// Solution 2
+/**
+ * We convert the number to a string, split it into an array, reverse the array, join the array back
+ * into a string, and then convert it back to a number
+ * @param n - the number to be reversed
+ * @returns The reversed integer.
+ */
+const reverseInt2 = (n) => {
+  const reversedInt = parseInt(n.toString().split('').reverse().join(''));
+
+  return n < 0 ? reversedInt * -1 : reversedInt;
+};
 
 module.exports = reverseInt;
