4e42e7931c0fd982f185a11cad26c095cc7fd7e7
ad0x99 <anhduc.dev99@gmail.com>
2022-09-03 14:03:11 +0700

    solved: fizzbuzz

--- The Coding Interview Bootcamp Algorithms & Data Structures/exercises/fizzbuzz/index.js
+++ The Coding Interview Bootcamp Algorithms & Data Structures/exercises/fizzbuzz/index.js
@@ -12,6 +12,24 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+/**
+ * For each number from 1 to n, if the number is divisible by 3 and 5, print 'fizzbuzz', if the number
+ * is divisible by 3, print 'fizz', if the number is divisible by 5, print 'buzz', otherwise print the
+ * number.
+ * @param n - the number to count up to
+ */
+const fizzBuzz = (n) => {
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('fizzbuzz');
+    } else if (i % 3 === 0) {
+      console.log('fizz');
+    } else if (i % 5 === 0) {
+      console.log('buzz');
+    } else {
+      console.log(i);
+    }
+  }
+};
 
 module.exports = fizzBuzz;
