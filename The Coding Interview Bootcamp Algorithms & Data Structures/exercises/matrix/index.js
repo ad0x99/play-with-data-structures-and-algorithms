9576cfd9b87d874dd9f43f06ee2b6e47b2c7945c
ad0x99 <anhduc.dev99@gmail.com>
2022-09-03 14:04:20 +0700

    solved: NxN spiral matrix

--- The Coding Interview Bootcamp Algorithms & Data Structures/exercises/matrix/index.js
+++ The Coding Interview Bootcamp Algorithms & Data Structures/exercises/matrix/index.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 // --- Directions
 // Write a function that accepts an integer N
 // and returns a NxN spiral matrix.
@@ -15,6 +16,57 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+// Solution 1
+/**
+ * We start from the top left corner and move right, then move down, then move left, then move up, and
+ * repeat until we've filled the entire matrix
+ * @param n - the number of rows and columns in the matrix
+ * @returns A matrix of numbers in a spiral pattern.
+ */
+const matrix = (n) => {
+  const result = [];
+  let startColumn = 0;
+  let endColumn = n - 1;
+  let startRow = 0;
+  let endRow = n - 1;
+  let counter = 1;
+  let i;
+
+  for (i = 0; i < n; i++) {
+    result.push([]);
+  }
+
+  while (startColumn <= endColumn && startRow <= endRow) {
+    // Top row
+    for (i = startColumn; i <= endColumn; i++) {
+      result[startRow][i] = counter;
+      counter++;
+    }
+    startRow++;
+
+    // Right column
+    for (i = startRow; i <= endRow; i++) {
+      result[i][endColumn] = counter;
+      counter++;
+    }
+    endColumn--;
+
+    // Bottom row
+    for (i = endColumn; i >= startColumn; i--) {
+      result[endRow][i] = counter;
+      counter++;
+    }
+    endRow--;
+
+    // Start column
+    for (i = endRow; i >= startRow; i--) {
+      result[i][startColumn] = counter;
+      counter++;
+    }
+    startColumn++;
+  }
+
+  return result;
+};
 
 module.exports = matrix;
