82038bb8245e885f2f3cfff1195018e5a7c178c4
ad0x99 <anhduc.dev99@gmail.com>
2022-09-03 14:03:11 +0700

    solved: capitalize first char of string

--- The Coding Interview Bootcamp Algorithms & Data Structures/exercises/capitalize/index.js
+++ The Coding Interview Bootcamp Algorithms & Data Structures/exercises/capitalize/index.js
@@ -7,6 +7,43 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+// Solution 1
+/**
+ * We split the string into an array of words, then we iterate over each word, capitalize the first
+ * letter, and push the word into a new array. Finally, we join the array of words back into a string
+ * @param str - The string to capitalize.
+ * @returns the capitalized version of the string.
+ */
+const capitalize = (str) => {
+  const arrayString = str.split(' ');
+  const capitalized = [];
+
+  for (const char of arrayString) {
+    capitalized.push(`${char.slice(0, 1).toUpperCase()}${char.slice(1)}`);
+  }
+
+  return capitalized.join(' ');
+};
+
+// Solution 2
+/**
+ * We start with the first letter capitalized, and then we iterate through the rest of the string,
+ * capitalizing the letter after a space
+ * @param str - the string to capitalize
+ * @returns the capitalized string.
+ */
+const capitalize2 = (str) => {
+  let capitalized = str[0].toUpperCase();
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === ' ') {
+      capitalized += str[i].toUpperCase();
+    } else {
+      capitalized += str[i];
+    }
+  }
+
+  return capitalized;
+};
 
 module.exports = capitalize;
