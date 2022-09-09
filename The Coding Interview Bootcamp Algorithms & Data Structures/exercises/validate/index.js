3e74247549831af782b686c490736259a2036524
ad0x99 <anhduc.dev99@gmail.com>
2022-09-09 21:04:17 +0700

    solved(ds): validate if a tree is binary search tree

--- The Coding Interview Bootcamp Algorithms & Data Structures/exercises/validate/index.js
+++ The Coding Interview Bootcamp Algorithms & Data Structures/exercises/validate/index.js
@@ -5,6 +5,32 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+/**
+ * If the node's data is greater than the max or less than the min, return false. Otherwise,
+ * recursively check the left and right nodes
+ * @param node - the current node we're validating
+ * @param [min=null] - the minimum value allowed in the tree
+ * @param [max=null] - the maximum value allowed in the tree
+ * @returns A boolean value.
+ */
+const validate = (node, min = null, max = null) => {
+  if (max !== null && node.data > max) {
+    return false;
+  }
+
+  if (min !== null && node.data < min) {
+    return false;
+  }
+
+  if (node.left && !validate(node.left, min, node.data)) {
+    return false;
+  }
+
+  if (node.right && !validate(node.right, node.data, max)) {
+    return false;
+  }
+
+  return true;
+};
 
 module.exports = validate;
