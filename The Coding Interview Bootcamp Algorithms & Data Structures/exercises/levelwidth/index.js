5b603158ce14521b920c51ef6e179ad475797ae9
ad0x99 <anhduc.dev99@gmail.com>
2022-09-09 20:27:42 +0700

    solved(ds): tree width with level width

--- The Coding Interview Bootcamp Algorithms & Data Structures/exercises/levelwidth/index.js
+++ The Coding Interview Bootcamp Algorithms & Data Structures/exercises/levelwidth/index.js
@@ -11,6 +11,29 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+/**
+ * We use a counter array to keep track of the number of nodes at each level. We use a sentinel value
+ * to keep track of when we've reached the end of a level
+ * @param root - the root node of the tree
+ * @returns An array of counters.
+ */
+const levelWidth = (root) => {
+  const counters = [0];
+  const array = [root, 'f'];
+
+  while (array.length > 1) {
+    const node = array.shift();
+
+    if (node === 'f') {
+      counters.push(0);
+      array.push('f');
+    } else {
+      array.push(...node.children);
+      counters[counters.length - 1]++;
+    }
+  }
+
+  return counters;
+};
 
 module.exports = levelWidth;
