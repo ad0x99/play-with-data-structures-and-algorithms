83ce627f3a4d81c8015e15103899a1cea62193a8
ad0x99 <anhduc.dev99@gmail.com>
2023-05-24 21:08:56 +0700

    refactor(js-algo-ds): add js config and common folder

--- JS Algorithms & Data Structures/Sorting Algorithms/Bubble Sort/bubble-sort.js
+++ JS Algorithms & Data Structures/Sorting Algorithms/Bubble Sort/bubble-sort.js
@@ -1,14 +1,4 @@
-/**
- * The function swaps the positions of two elements in an array.
- * @param array - The array parameter is an array that contains elements to be swapped.
- * @param idx1 - `idx1` is a parameter representing the index of the first element to be swapped in the
- * `array`.
- * @param idx2 - `idx2` is the index of the second element in the array that we want to swap with the
- * element at `idx1`.
- */
-const swap = (array, idx1, idx2) => {
-  [array[idx1], array[idx2]] = [array[idx2], array[idx1]];
-};
+import { swap } from '../common/swap.js';
 
 /**
  * Pseudocode - Bubble Sort
