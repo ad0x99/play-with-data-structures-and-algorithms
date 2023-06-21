79491a540803a203fd87194e01bf70203dc7106a
ad0x99 <50711511+ad0x99@users.noreply.github.com>
2023-06-21 21:46:48 +0700

    feat(js-algo-ds/doubly-linked-list): add getting method

--- JS Algorithms & Data Structures/Data Structures/Linked Lists/Doubly Linked List/example.js
+++ JS Algorithms & Data Structures/Data Structures/Linked Lists/Doubly Linked List/example.js
@@ -53,3 +53,17 @@ console.log(`tail: ${list.tail.value}`);
 console.log(`head.next: ${list.head.next.value}`);
 console.log(`tail.previous: ${list.tail.previous.value}`);
 console.log('======Unshift======');
+
+console.log('======Get======');
+const getFirstItem = list.get(0);
+const getLastItem = list.get(2);
+
+console.log(`getFirstItem: ${getFirstItem.value}`);
+console.log(`getLastItem: ${getLastItem.value}`);
+list.print();
+
+console.log(`head: ${list.head.value}`);
+console.log(`tail: ${list.tail.value}`);
+console.log(`head.next: ${list.head.next.value}`);
+console.log(`tail.previous: ${list.tail.previous.value}`);
+console.log('======Get======');
