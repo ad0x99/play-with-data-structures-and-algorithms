63fad53e38e6e8b939d746e1d5106143a624b5df
ad0x99 <50711511+ad0x99@users.noreply.github.com>
2024-03-11 15:17:18 +0700

    feat(leetcode): add solution of two sum with comment enhancement

--- LeetCode/Easy/twoSum.js
+++ LeetCode/Easy/twoSum.js
@@ -1,35 +1,29 @@
 /**
- * The function `twoSum` takes an array of numbers `nums` and a target number `target`, and returns an
- * array of two indices where the numbers at those indices in `nums` add up to the target.
- * @param nums - An array of numbers.
- * @param target - The target parameter is the desired sum that we want to find using two numbers from
- * the nums array.
- * @returns The function `twoSum` returns an array containing the indices of the two numbers in the
- * `nums` array that add up to the `target` value.
+ * https://leetcode.com/problems/two-sum/description/
  *
- * Time complexity: O(n)
- * Space complexity: O(1)
+ * This solution is suitable for unsorted array
+ *
+ * The time complexity of this solution is O(n) because we iterate through the array of numbers only once. The space complexity is also O(n) because we use a hashmap to store encountered numbers, which can potentially store all the numbers in the input array.
+ *
+ * The use of a hashmap allows us to achieve a linear time complexity by trading off space complexity. The hashmap enables us to quickly look up whether a complement number (target - current number) has been encountered before in O(1) time. This efficient lookup operation helps us find the indices of the two numbers that add up to the target sum without needing to iterate through the array multiple times.
  */
-const twoSum = (nums, target) => {
+const twoSumWithHashmap = (nums, target) => {
+  // We use a hashmap to store encountered number
   let temp = {};
 
+  // Iterate through the array
   for (let i = 0; i < nums.length; i++) {
-    /* Checks if the current number `nums[i]` exists as a key in the
-   `temp` object. If it does, it means that we have already encountered a number in the `nums` array
-   that, when added to the current number, gives the target sum. In this case, we return an array
-   containing the index of the previously encountered number (stored as the value in the `temp`
-   object) and the current index `i`. This gives us the indices of the two numbers in the `nums`
-   array that add up to the target value. */
+    // Checks if the current number exists as a key in the hashmap.
+    // If it does, it means that we have already encountered a number in the array that, when added to the current number, gives the target sum. In this case, we return an array containing the index of the previously encountered number and the current index.
     if (temp[nums[i]] >= 0) {
       return [temp[nums[i]], i];
     }
 
-    /* Store the index `i` of the current number `nums[i]`
-    in the `temp` object. */
+    // Store the index `i` of the current number in the hashmap.
     temp[target - nums[i]] = i;
   }
 };
 
-console.log(twoSum([2, 11, 7, 15], 9));
-console.log(twoSum([3, 2, 4], 6));
-console.log(twoSum([3, 3], 6));
+console.log(twoSumWithHashmap([2, 11, 7, 15], 9)); // [0, 1]
+console.log(twoSumWithHashmap([3, 2, 4], 6)); // [1, 2]
+console.log(twoSumWithHashmap([3, 3], 6)); // [0, 1]
