06408a23ad553bbdeceb48f996584582791b4383
ad0x99 <50711511+ad0x99@users.noreply.github.com>
2024-07-05 15:35:13 +0700

    feat(leetcode): add rust solution for maximum size subarray sum equals k

--- LeetCode/Medium/maximumSizeSubarraySumEqualsK.js
+++ LeetCode/Medium/maximumSizeSubarraySumEqualsK.js
@@ -2,13 +2,15 @@
  * https://leetcode.com/problems/maximum-size-subarray-sum-equals-k/description/
  * https://www.lintcode.com/problem/911/
  *
- *  * Given an array nums and a target value k, find the maximum length of a subarray that sums to k. If there isn't one, return 0 instead.
+ * Given an array nums and a target value k, find the maximum length of a subarray that sums to k. If there isn't one, return 0 instead.
  *
  * Hash Table + Prefix Sum Approach
  *
- * We have prefixSum[i] = nums[0...i] => If we can found a j position such that prefixSum[i] - prefixSum[j] = k => sum(nums[j+1...i]) = k.
+ * We have `prefixSum[i] = nums[0...i]` => If we can found a j position such that `prefixSum[i] - prefixSum[j] = k` => `sum(nums[j+1...i]) = k`.
  *
  * Normal case:
+ * 
+ * ```js
  * - nums =       [4, 1, -1, 5, -2, 3], k = 3
  *                 j             i
  * - prefixSum =  [4, 5, 4,  9, 7, 10]
@@ -17,7 +19,7 @@
  * - => prefixSum[4] - prefixSum[0] = 7 - 4 = 3 = k
  * - => sum(nums[j+1...i]) = sum(nums[1...4]) = k
  * - => longest = i - j = 4 - 0 = 4
- *
+ *```
  *
  * Special case: nums = [1, -1, 5, -2, 3], k = 3
  *          prefixSum = [1, 0, 5, 3, 6]
@@ -28,17 +30,24 @@
  *
  *
  * 1.  We create a new Map object (seen) which will store prefix sums encountered so far and their corresponding indices in the nums list.
+ *
  * 2. We initialize a variable `prefixSum` to keep track of the running prefix sum throughout the loop. and a variable `ans` to store the maximum length of the subarray found so far.
+ * 
  * 3. We insert a special case into the seen map. It indicates that an empty subarray (with sum `0`) starts at index `-1` (since it doesn't exist in the actual array). This helps handle cases where the subarray starts from the index of zero.
+ * 
  * 4. We iterate through each element (`nums[i]`) in the nums list.
+ * 
  * 5. We update the `prefixSum` by adding the current element to the previous sum. This represents the cumulative sum up to the current index.
+ * 
  * 6. And then, we calculate the complement (or potential ending sum) for the subarray. This represents the sum we're looking for in a subarray that ends before the current element (i) such that the difference between the current `prefixSum` and this ending sum would be equal to the target sum (k).
+ * 
  * 7. If the `complement` (representing the potential ending sum of a subarray) exists in the `seen` map, it means there's a subarray ending before the current element (i) that has a sum equal to complement.
  * - 7.1: We can then calculate the length of this subarray by subtracting the index of the ending sum (`seen.get(complement)`) from the current index (`i`).
  *
  * 8. After that, we update the ans (maximum length) if the calculated subarray length is greater than the current maximum.
+ * 
  * 9. In another case, if the current `prefixSum` doesn't exist in the `seen` map, it means we're encountering this prefix sum for the first time.
- * - 9.1: We update the `seen` map with the current `prefixSum` and its corresponding index (`i`). This is used for future lookups during the loop to identify potential subarrays that could end with this prefixSum.
+ * - 9.1: We update the `seen` map with the current `prefixSum` and its corresponding index (`i`). This is used for future lookups during the loop to identify potential sub-arrays that could end with this prefixSum.
  *
  * 10. After iterating through all elements, we return the `ans` which represents the maximum length of the subarray found in the nums list.
  *
